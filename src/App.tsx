24fd6695dd375007a9dcf921b0b0ec7b143eef51
ManoPedra <thales.licinio05@gmail.com>
2024-05-26 00:37:34 -0300

    Componente Completado e começando estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 
 import './App.css'
-
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import { CompoMenu } from './components/Componente1'
 function App() {
-  
+  let algo
+
+  function AcaoOK(){
+      console.log('DEU CERTO')
+  }
+
+  function AcaoNegar(){
+      console.log('TAMBEM DEU CERTO')
+  } 
+
 
   return (
     <>
-        <CompoMenu>
-
-        </CompoMenu>
+        <CompoMenu texto='algo teste' icone={QuestionMarkIcon} cancela={AcaoNegar} aceita={AcaoOK} />
     </>
   )
 }
 
 export default App
+
+
--- src/components/Componente1.tsx
+++ src/components/Componente1.tsx
@@ -1,19 +1,44 @@
 import { ElementType } from "react"
+import CheckIcon from '@mui/icons-material/Check';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import CloseIcon from '@mui/icons-material/Close';
+
+
+
+
+
 import './Compo1.css'
 interface MsgPropri{
     icone:ElementType,
     texto: string
+    aceita:()=> any,
+    cancela:()=> any,
+    Opcional?: ()=> any,
 }
 
-export function CompoMenu({icone , texto}:MsgPropri){
+export function CompoMenu({icone:Icone, cancela , texto, aceita}:MsgPropri){
 
     return(
-        <>
+       
          <div className="Fi1">
-            
+         
               <label htmlFor="Fi1">Mensagem Alerta!</label> 
+              <HighlightOffIcon className=""/>
+                 
+               
+                <div className="CompoBody" >    
+                  <Icone/>
+                    <p>{texto}</p>
+                    <button onClick={aceita}><CheckIcon/></button>
+                  <button onClick={cancela}><CloseIcon/></button>
+                </div>
+
+                <div className="CompoFooter">
+
+                </div>
+                
                 
          </div>
-        </>
+      
     )
 }
